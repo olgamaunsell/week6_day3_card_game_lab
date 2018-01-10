ba1dd89c7f2c97d2e7cfa11f129feb54a5156522
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-10 20:11:18 +0000

    Player class - 3 tests passed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,15 +2,23 @@ import java.util.ArrayList;
 
 public class Player {
 
+    private String name;
     private ArrayList<Card> hand;
 
-    public Player(){
+    public Player(String name){
+        this.name = name;
         this.hand = new ArrayList<>();
-
     }
-
 
     public int cardCount() {
         return this.hand.size();
+    }
+
+    public void receiveCard(Card dealtCard) {
+        hand.add(dealtCard);
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -5,17 +5,37 @@ import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
 
-    private Player player;
+    private Player player1;
+    private Player player2;
+    private Card card1;
+    private Card card2;
 
     @Before
 
     public void before(){
-        player = new Player();
+        player1 = new Player("Olga");
+        player2 = new Player("Elvis");
+        card1 = new Card(Suit.DIAMONDS, Rank.NINE);
+        card2 = new Card(Suit.HEARTS, Rank.QUEEN);
+    }
+
+    @Test
+
+    public void hasName(){
+        assertEquals("Olga", player1.getName());
     }
 
     @Test
 
     public void playerHandStartsEmpty(){
-        assertEquals(0, player.cardCount());
+        assertEquals(0, player1.cardCount());
     }
+
+    @Test
+
+    public void canReceiveCard(){
+        player1.receiveCard(card1);
+        assertEquals(1, player1.cardCount());
+    }
+
 }
